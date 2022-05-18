dce8b208af798f8e6ae520ea21c459f236162aab
Sourcery AI <>
2022-05-18 06:24:38 +0000

    'Refactored by Sourcery'

--- processguardium.py
+++ processguardium.py
@@ -35,8 +35,7 @@ def main():
         f = open(r'log.txt', 'w')
         f.close()
     with open(r'config.ini', 'r') as f:
-        for line in f.readlines():
-            guardlist.append(line.strip())  # 每一行
+        guardlist.extend(line.strip() for line in f.readlines())
     # 读取配置文件到3个list
     for line in guardlist:  # ！！！！！！
         if len(line.split(',')) == 2:
@@ -58,10 +57,10 @@ def main():
     while True:
         for key, name in enumerate(namelist):  # 期待运行的进程
             if (name not in current_runing_process) and (name not in [i.split('.')[0] for i in current_runing_process]):
-                os.popen(processlist[key] + ' ' + arglist[key])
+                os.popen(f'{processlist[key]} {arglist[key]}')
                 with open(r'log.txt', 'a+', encoding='utf-8') as f:
                     f.write(str(datetime.now()) + ' 进程 %s 未启动，正在强制启动\n' % name)
-                #current_runing_process = checkprocess()
+                            #current_runing_process = checkprocess()
         time.sleep(1)
 
 
